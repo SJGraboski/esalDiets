c93700bd447ac0e7a4e09029c8bd7844ccb892cd
Steven Graboski <sgrabosk@gmail.com>
2016-07-03 13:42:32 -0400

    Initial server.js file created.

---
+++ server.js
@@ -0,0 +1,59 @@
+/* Esal - Diet App
+ * Jerome, Greg, Racquel, Steve
+ * -/-/-/-/-/-/-/-/-/-/-/-/-/-/-/ */
+
+// dependencies
+// ============
+var express = require('express');
+var bodyParser = require('body-pasers');
+var logger = require('morgan');
+
+
+
+// Express
+// =======
+
+// instantiate our express
+var app = express();
+app.use(logger('dev'));
+
+// set up bodyparser
+app.use(bodyParser.json);
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// set up the public directory as our static folder
+var staticContentFolder = __dirname + 'public';
+app.use(express.static(staticContentFolder));
+
+
+
+// Sequelize
+// =========
+
+// bring in our sequelize models 
+var models = require('./models');
+
+// and sync them with our db
+models.sequelize.sync();
+
+
+// API routing
+// ===========
+
+// Note: react and react-router will handle html routing
+require('./routes/api.js')(app);
+// auth route to be added once we get to login
+
+
+// Listen
+// ======
+
+// define our port (either our environment's preset, or 3000)
+var PORT = process.env.PORT || 3000
+
+// listen on our port
+app.listen(PORT, function(){
+	console.log('Esal is listening. Port: ' + PORT);
+})
