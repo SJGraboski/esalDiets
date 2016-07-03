1d4bd8a47c99f6ab69222c67890824edc6442f4f
Steven Graboski <sgrabosk@gmail.com>
2016-07-03 15:45:51 -0400

    Skeleton created

---
+++ config/routes.js
@@ -0,0 +1,12 @@
+// react routes
+// ============
+
+// dependencies
+var React = require('react');
+
+// We will then pull the router 
+var Router = require('react-router');
+var Route = Router.Route;
+
+//  This is the catchall route
+var IndexRoute	= Router.IndexRoute;
