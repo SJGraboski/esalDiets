d7678f12a074965bfec659768d3558e12cfe2d4c
Steven Graboski <sgrabosk@gmail.com>
2016-07-16 13:06:04 -0400

    Changed api query, started coding new parent components

--- gulpfile.js
+++ gulpfile.js
@@ -11,32 +11,45 @@ var webpack = require('gulp-webpack'); // for webpack operation
 var nodemon = require('gulp-nodemon'); // for nodemon
 
 
-// create the default gulp task 
-// (which can be run by typing "gulp" as the lone cli argument)
-gulp.task('default', function() {
+gulp.task('install', function(cb){
+  // gulp src: if package.json is in the file stream, run this
+  gulp.src("./package.json")
 
-	// gulp src: if package.json is in the file stream, run this
-	gulp.src("./package.json")
-
-		// pipe install function to package.json, 
+    // pipe install function to package.json, 
     // effectively installing all packages in package.json
-	  .pipe(install());
+    .pipe(install());
+  // will not run callback if error occurs
+  cb();
+})
 
-	// if there's an app/app.js...
-	gulp.src('./app/app.js')
-		// ...then get the webpack config file and run it
+gulp.task('webpack', ['install'], function(cb){
+  // check if app.js exists
+  gulp.src('./app/app.js')
+    // ...then get the webpack config file and run it
     .pipe(webpack( require('./webpack.config.js') ))
     // We don't set up a destination file, since it's established in webpack.config.js
-    .pipe(gulp.dest(''))
+    .pipe(gulp.dest(''));
+  // if error, don't update the callback
+  cb();
+})
 
-  // run the nodemon gulp plugin
+gulp.task('nodemon', ['install', 'webpack'], function(cb){
+  // run nodemon
   nodemon({
-  	// run server.js with nodemon
+    // run server.js with nodemon
     script: 'server.js',
     // update the server whenever the dev updates a js or html file 
     // in the project directory
     ext: 'js html',
     // run in dev mode
-  	env: { 'NODE_ENV': 'development' }
+    env: { 'NODE_ENV': 'development' }
   })
-});
+  // run the callback
+  cb();
+})
+
+
+  
+// create the default gulp task 
+// (which can be run by typing "gulp" as the lone cli argument)
+gulp.task('default', ['install', 'webpack', 'nodemon']);
