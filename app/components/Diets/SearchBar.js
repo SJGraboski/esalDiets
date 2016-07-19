3877ec1e930bec791de5422ba9f37d8e691890f2
jcarless <carless.jerome@gmail.com>
2016-07-18 20:36:28 -0400

    diet search components added

---
+++ app/components/Diets/SearchBar.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+    constructor(props){
+        super(props);
+        this.state = {term: ''};
+
+    }
+    render() {
+        return (
+            <div className="input-group">
+                <input type="text" className="form-control dietSearch" id="dietSearch" placeholder="Diet Search"
+                       value={this.state.term}
+                       onChange= {event => this.onInputChange(event.target.value) }/>
+                <span className="input-group-btn">
+                    <button className="btn btn-default dietSearchBTN" id="dietSearchBTN" type="button">
+                        <i className="fa fa-search" aria-hidden="true"></i></button>
+                </span>
+            </div>
+
+        );
+    }
+
+    onInputChange(term) {
+        this.setState({term});
+        this.props.onSearchTermChange(term);
+    }
+
+}
+
+
+module.exports = SearchBar;
