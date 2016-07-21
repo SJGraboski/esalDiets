0fead62085b7e2852b741d9c1e3d9ba41cb7c37d
jcarless <carless.jerome@gmail.com>
2016-07-20 21:03:40 -0400

    component switching functioning

--- app/components/App.js
+++ app/components/App.js
@@ -5,6 +5,8 @@ var Navigation = require('react-router').Navigation;
 import _ from 'lodash';
 import helpers from '../utils/helpers';
 var PropTypes = React.PropTypes;
+import DietList from './Diets/DietList';
+import DietProfile from './Diets/DietProfile';
 
 
 
@@ -32,17 +34,13 @@ var App = React.createClass({
 		var self = this;
 		helpers.getSearchResults(term)
 		.then(function(diets){
-			var dietName = diets.data[0].name;
-			console.log('result: ' + diets.data[0].name)
-			// updateDiet(dietName);
 			self.setState({
 				diets: diets.data,
 				selectedDiet: diets.data[0]
 			});
 			console.log(self.state.diets)
+			console.log(self.state.selectedDiet)
 		})
-
-
 	},
 
 	// Allow for transitions between elements.
@@ -52,6 +50,65 @@ var App = React.createClass({
 	// main component app. Takes in the other routes
 	render: function() {
 		const dietSearch = _.debounce((term) => { this.dietSearch(term)}, 300);
+
+		if(!this.state.selectedDiet){
+			return (
+				<div>
+					<div className="container" id="main">
+
+						<header className="masthead">
+							<div className="container">
+								<div className="row">
+									<div>
+										<h1 className="dietName">ESAL</h1>
+									</div>
+								</div>
+							</div>
+						</header>
+
+						<div className="nav-wrapper">
+							<div id="nav">
+								<nav className="navbar navbar-default navbar-static">
+									<div className="container">
+
+
+										<div className="navbar-header">
+											<a className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+												<span className="sr-only">Toggle navigation</span>
+												<span className="icon-bar"></span>
+												<span className="icon-bar"></span>
+												<span className="icon-bar"></span>
+											</a>
+											<a href="#" className="navbar-left"><img className="navbar-left" src="./assets/images/esallogosmall.png" id="navlogo" /></a>
+										</div>
+
+
+										<div className="navbar-collapse collapse">
+											<ul className="nav navbar-nav navbar-right icon">
+												<li><a href="#analytics"><i className="fa fa-line-chart" aria-hidden="true"></i> Analytics</a></li>
+												<li><a href="#userdata"><i className="fa fa-user" aria-hidden="true"></i> User Data</a></li>
+											</ul>
+											<SearchBar
+												onSearchTermChange={dietSearch} />
+
+										</div>
+
+									</div>
+								</nav>
+							</div>
+						</div>
+					</div>
+
+
+
+					<div className="container" id="childrenContainer">
+						{this.props.children}
+					</div>
+					<div id="placeholder"></div>
+				</div>
+			)
+		}
+
 
 
 		return (
@@ -104,7 +161,10 @@ var App = React.createClass({
 
 
 			<div className="container" id="childrenContainer">
-				{this.props.children}
+				<DietProfile diet={this.state.selectedDiet} />
+				<DietList
+					onDietSelect={selectedDiet => this.setState({selectedDiet}) }
+					diets={this.state.diets} />
 			</div>
 			<div id="placeholder"></div>
 			</div>
